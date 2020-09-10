18c0b109922edefc836fbfb1263edc661312095f
odoo <Loina7723>
2020-09-10 23:37:23 +0800

    salary bar chart

--- app/src/main/java/com/example/homerenting_prototype_one/bouns/Bonus_View.java
+++ app/src/main/java/com/example/homerenting_prototype_one/bouns/Bonus_View.java
@@ -4,6 +4,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -23,6 +24,14 @@ import com.example.homerenting_prototype_one.order.Order;
 import com.example.homerenting_prototype_one.setting.Setting;
 import com.example.homerenting_prototype_one.system.System;
 import com.example.homerenting_prototype_one.valuation.Valuation;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 
 import java.util.ArrayList;
 
@@ -41,6 +50,8 @@ public class Bonus_View extends AppCompatActivity {
 
     ArrayList<String[]> data;
     ArrayList<String[]> emp = new ArrayList<>();
+    ArrayList<String> employee_names;
+    ArrayList<Integer> salaries;
 
     private Context context = this;
     String TAG = "Bonus_View";
@@ -70,8 +81,6 @@ public class Bonus_View extends AppCompatActivity {
         });
 
         setChart(); //圖表
-
-
 
         data = new ArrayList<>();
         int i;
@@ -137,7 +146,8 @@ public class Bonus_View extends AppCompatActivity {
         final Dialog dialog = new Dialog(context);
         dialog.setContentView(R.layout.bonus_dialog);
 
-        setList(dialog);
+//        setList(dialog);
+        setBarChart(dialog);
 
         Button dialog_btn = dialog.findViewById(R.id.check_dialog_btn);
         dialog_btn.setOnClickListener(new View.OnClickListener() {
@@ -158,6 +168,70 @@ public class Bonus_View extends AppCompatActivity {
                 dialog.show();
             }
         } );
+    }
+
+    private void setBarChart(Dialog dialog){
+        HorizontalBarChart hBarChart = dialog.findViewById(R.id.hBarChart_BD);
+
+        BarDataSet barDataSet = new BarDataSet(getBarData(), ""); //放入資料(資料List, 該資料的標籤名稱)
+        barDataSet.setValueTextSize(20); //數值文字大小
+        barDataSet.setValueFormatter(new IndexAxisValueFormatter(){ //去掉數值小數點
+            @Override
+            public String getFormattedValue(float value) {
+                if(value == 0) return "";
+                return String.valueOf((int) value);
+            }
+        });
+        barDataSet.setDrawValues(true); //在bar上顯示數值
+        barDataSet.setColor(Color.parseColor("#FB8527")); //bar的顏色
+        barDataSet.setValueTextColor(Color.parseColor("#FFFFFF")); //數值文字顏色
+
+        BarData barData = new BarData(barDataSet);
+        barData.setHighlightEnabled(false); //不能點擊反光
+
+        XAxis xAxis = hBarChart.getXAxis(); //X軸
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM); //X軸的位置(放下面)
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(employee_names)); //將x軸的值當labels的key
+        xAxis.setLabelCount(employee_names.size()); //X軸刻度數量
+        xAxis.setDrawGridLines(false); //取消垂直網格線
+        xAxis.setTextSize(20); //標籤文字大小(sp)
+
+        Legend legend = hBarChart.getLegend(); //圖例
+        legend.setEnabled(false); //不顯示圖例
+
+        hBarChart.getAxisRight().setEnabled(false); //取消右側(下方)Y軸
+        hBarChart.getAxisLeft().setEnabled(false); //取消左側(下方)Y軸
+        hBarChart.getDescription().setEnabled(false); //不顯示描述文字
+        hBarChart.setDrawValueAboveBar(false); //數值放在bar上方(true:above bar)或頂端(false:top)
+
+        hBarChart.setData(barData); //圖表放入資料
+    }
+
+    private ArrayList getBarData(){
+        employee_names = set_employee_name();
+        salaries = set_salaries();
+
+        ArrayList<BarEntry> yValue = new ArrayList<>(); //資料List
+        for(int x = 0; x < employee_names.size(); x++){
+            yValue.add(new BarEntry(x, salaries.get(x))); //加入新資料
+        }
+        return yValue;
+    }
+
+    private ArrayList set_employee_name(){
+        employee_names = new ArrayList<>();
+        employee_names.add("");
+        employee_names.add("一號");
+        employee_names.add("二號");
+        return employee_names;
+    }
+
+    private ArrayList set_salaries(){
+        salaries = new ArrayList<>();
+        salaries.add(0);
+        salaries.add(55000);
+        salaries.add(25000);
+        return salaries;
     }
 
     private void setList(Dialog dialog){
