8f4a5abf9bd5bea1d5dfb6a6f35dfcd7d1d076d3
Sh4dy <marshall871018@gmail.com>
2021-04-25 18:27:46 +0800

    CarWeight Spinner past time cannot choose Register logic check SQLite construct(open, close, and onUpgrade) 優惠折扣的bug ArrayOutOfBounds還沒想到解決辦法

--- app/src/main/java/com/example/homerenting_prototype_one/helper/DatabaseHelper.java
+++ app/src/main/java/com/example/homerenting_prototype_one/helper/DatabaseHelper.java
@@ -1,11 +1,24 @@
 package com.example.homerenting_prototype_one.helper;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteCantOpenDatabaseException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+
+
 import static com.example.homerenting_prototype_one.model.TableContract.CompanyTable.SQL_CREATE_COMPANY;
 import static com.example.homerenting_prototype_one.model.TableContract.CompanyTable.SQL_DELETE_COMPANY;
+import static com.example.homerenting_prototype_one.model.TableContract.ChooseTable.SQL_CREATE_CHOOSE;
+import static com.example.homerenting_prototype_one.model.TableContract.ChooseTable.SQL_DELETE_CHOOSE;
+import static com.example.homerenting_prototype_one.model.TableContract.CommentsTable.SQL_CREATE_COMMENTS;
+import static com.example.homerenting_prototype_one.model.TableContract.CommentsTable.SQL_DELETE_COMMENTS;
+import static com.example.homerenting_prototype_one.model.TableContract.StaffTable.SQL_CREATE_STAFF;
+import static com.example.homerenting_prototype_one.model.TableContract.StaffTable.SQL_DELETE_STAFF;
+import static com.example.homerenting_prototype_one.model.TableContract.VehicleTable.SQL_CREATE_VEHICLE;
+import static com.example.homerenting_prototype_one.model.TableContract.VehicleTable.SQL_DELETE_VEHICLE;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
@@ -16,13 +29,55 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL(SQL_CREATE_COMPANY);
+    public void onCreate(SQLiteDatabase db) throws SQLiteCantOpenDatabaseException {
+        /*db.execSQL(SQL_CREATE_COMPANY);
+        db.execSQL(SQL_CREATE_CHOOSE);
+        db.execSQL(SQL_CREATE_COMMENTS);
+        db.execSQL(SQL_CREATE_STAFF);
+        db.execSQL(SQL_CREATE_VEHICLE);*/
     }
 
-    @Override
+    @Override /*此方法會將舊TABLE全部刪除再新增新的TABLE，資料庫版本要更新*/
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL(SQL_DELETE_COMPANY);
-        onCreate(db);
+        if(newVersion > oldVersion){
+            db.beginTransaction();
+            boolean success = false;
+
+            switch (oldVersion){
+                case 1:
+                 /*更新table欄位SQL指令
+                 *
+                 * UPDATE指令
+                 *
+                 * */
+                 oldVersion++; //資料庫版本更新
+                 success = true;
+                 break;
+            }
+             if(success){
+                 db.setTransactionSuccessful();
+             }
+             db.endTransaction();
+        }
+        else{
+            onCreate(db);
+        }
+        /*db.execSQL(SQL_DELETE_COMPANY);
+        db.execSQL(SQL_DELETE_CHOOSE);
+        db.execSQL(SQL_CREATE_COMMENTS);
+        db.execSQL(SQL_DELETE_COMMENTS);
+        db.execSQL(SQL_DELETE_STAFF);
+        db.execSQL(SQL_DELETE_VEHICLE);*/
+
+    }
+
+
+    @Override
+    public void onOpen(SQLiteDatabase db){
+        super.onOpen(db);
+    }
+
+    public void onClose(SQLiteDatabase db){
+        super.close();
     }
 }
--- app/src/main/java/com/example/homerenting_prototype_one/model/TableContract.java
+++ app/src/main/java/com/example/homerenting_prototype_one/model/TableContract.java
@@ -16,12 +16,12 @@ public class TableContract {
         public static final String COLUMN_NAME_URL = "url";
         public static final String COLUMN_NAME_EMAIL = "email";
         public static final String COLUMN_NAME_LINE_ID = "line_id";
-        public static final String COLUMN_NAME_PHILOSOPHY = "pyilosophy";
+        public static final String COLUMN_NAME_PHILOSOPHY = "philosophy";
         public static final String COLUMN_NAME_LAST_DISTRIBUTION = "last_distribution";
 
         public static final String SQL_CREATE_COMPANY =
-                "CREATE TABLE "+TABLE_NAME+" ( "+
-                        COLUMN_NAME_COMPANY_ID+" int(10) NOT NULL, "+
+                "CREATE TABLE IF NOT EXISTS "+TABLE_NAME+" ( "+
+                        COLUMN_NAME_COMPANY_ID+" int(10) NOT NULL AUTO_INCREMENT, "+
                         COLUMN_NAME_COMPANY_NAME+" varchar(100), "+
                         COLUMN_NAME_IMG+" varchar(99), "+
                         COLUMN_NAME_ADDRESS+" varchar(50), "+
@@ -52,7 +52,7 @@ public class TableContract {
         public static final String COLUMN_NAME_VALUATION_STATUS = "valuaiton_status";
 
         public static final String SQL_CREATE_CHOOSE = "" +
-                "CREATE TABLE "+TABLE_NAME+" ( "+
+                "CREATE TABLE IF NOT EXISTS "+TABLE_NAME+" ( "+
                 COLUMN_NAME_ORDER_ID+" int(10) NOT NULL, "+
                 COLUMN_NAME_COMPANY_ID+" int(10) NOT NULL, "+
                 COLUMN_NAME_VALUATION_DATE+" date, "+
@@ -68,6 +68,94 @@ public class TableContract {
                 ");";
 
         public static final String SQL_DELETE_CHOOSE =
+                "DROP TABLE IF EXISTS "+ TABLE_NAME;
+    }
+
+    public static class CommentsTable implements BaseColumns{
+        public static final String TABLE_NAME = "comments";
+        public static final String COLUMN_NAME_COMMENT_ID = "comment_id";
+        public static final String COLUMN_NAME_ORDER_ID = "order_id";
+        public static final String COLUMN_NAME_MEMBER_ID = "member_id";
+        public static final String COLUMN_NAME_COMPANY_ID = "company_id";
+        public static final String COLUMN_NAME_COMMENT_DATE = "comment_date";
+        public static final String COLUMN_NAME_SERVICE_QUALITY = "service_quality";
+        public static final String COLUMN_NAME_WORK_ATTITUDE = "work_attitude";
+        public static final String COLUMN_NAME_COMMENT = "comment";
+        public static final String COLUMN_NAME_REPLY = "reply";
+
+        public static final String SQL_CREATE_COMMENTS = "" +
+                "CREATE TABLE IF NOT EXISTS "+TABLE_NAME+" ( "+
+                COLUMN_NAME_COMMENT_ID+" int(10) NOT NULL AUTO_INCREMENT, "+
+                COLUMN_NAME_ORDER_ID+" int(10) NOT NULL, "+
+                COLUMN_NAME_MEMBER_ID+" int(10), "+
+                COLUMN_NAME_COMPANY_ID+" int(10), "+
+                COLUMN_NAME_COMMENT_DATE+" timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(), "+
+                COLUMN_NAME_SERVICE_QUALITY+" float(10) NOT NULL CHECK (service_quality between 0 and 5), "+
+                COLUMN_NAME_WORK_ATTITUDE+" float(10) NOT NULL  CHECK (work_attitude between 0 and 5), "+
+                COLUMN_NAME_COMMENT+" varchar(300) CHARACTER SET utf8 DEFAULT NULL, "+
+                COLUMN_NAME_REPLY+" varchar(300) CHARACTER SET utf8 DEFAULT NULL, "+
+                "PRIMARY KEY (comment_id), "+
+                "FOREIGN KEY (order_id), "+
+                "REFERENCES orders(order_id) ON DELETE CASCADE, "+
+                "FOREIGN KEY (member_id) "+
+                "REFERENCES member(member_id) ON DELETE CASCADE, "+
+                "FOREIGN KEY (company_id), "+
+                "REFERENCES company(company_id) ON DELETE CASCADE "+
+                ");";
+
+        public static final String SQL_DELETE_COMMENTS =
+                "DROP TABLE IF EXISTS "+ TABLE_NAME;
+    }
+    public static class StaffTable implements BaseColumns{
+        public static final String TABLE_NAME = "staff";
+        public static final String COLUMN_NAME_STAFF_ID = "staff_id";
+        public static final String COLUMN_NAME_STAFF_NAME = "staff_name";
+        public static final String COLUMN_NAME_COMPANY_ID = "company_id";
+        public static final String COLUMN_NAME_START_TIME = "start_time";
+        public static final String COLUMN_NAME_END_TIME = "end_time";
+
+        public static final String SQL_CREATE_STAFF = "" +
+                "CREATE TABLE IF NOT EXISTS "+TABLE_NAME+" ( "+
+                COLUMN_NAME_STAFF_ID+" int(10) NOT NULL AUTO_INCREMENT, "+
+                COLUMN_NAME_STAFF_NAME+" varchar(50) CHARACTER SET utf8, "+
+                COLUMN_NAME_COMPANY_ID+" int(10) NOT NULL, "+
+                COLUMN_NAME_START_TIME+" timestamp NOT NULL DEFAULT current_timestamp(), "+
+                COLUMN_NAME_END_TIME+" datetime DEFAULT NULL, "+
+                "PRIMARY KEY (staff_id), "+
+                "FOREIGN KEY (company_id), "+
+                "REFERENCES company(company_id) ON DELETE CASCADE "+
+                ");";
+
+        public static final String SQL_DELETE_STAFF =
+                "DROP TABLE IF EXISTS "+ TABLE_NAME;
+
+    }
+
+    public static class VehicleTable implements BaseColumns{
+        public static final String TABLE_NAME = "vehicle";
+        public static final String COLUMN_NAME_VEHICLE_ID = "vehicle_id";
+        public static final String COLUMN_NAME_PLAT_NUM = "plat_num";
+        public static final String COLUMN_NAME_VEHICLE_WEIGHT = "vehicle_weight";
+        public static final String COLUMN_NAME_VEHICLE_TYPE = "vehicle_type";
+        public static final String COLUMN_NAME_COMPANY_ID = "company_id";
+        public static final String COLUMN_NAME_START_TIME = "start_time";
+        public static final String COLUMN_NAME_END_TIME = "end_time";
+
+        public static final String SQL_CREATE_VEHICLE = "" +
+                "CREATE TABLE IF NOT EXISTS "+TABLE_NAME+" ( "+
+                COLUMN_NAME_VEHICLE_ID+" int(10) NOT NULL AUTO_INCREMENT, "+
+                COLUMN_NAME_PLAT_NUM+" varchar(99) NOT NULL, "+
+                COLUMN_NAME_VEHICLE_WEIGHT+" varchar(10), "+
+                COLUMN_NAME_VEHICLE_TYPE+" varchar(99) CHARACTER SET utf8, "+
+                COLUMN_NAME_COMPANY_ID+" int(10) NOT NULL, "+
+                COLUMN_NAME_START_TIME+" timestamp NOT NULL DEFAULT current_timestamp(), "+
+                COLUMN_NAME_END_TIME+" datetime DEFAULT NULL, "+
+                "PRIMARY KEY (vehicle_id), "+
+                "FOREIGN KEY (company_id), "+
+                "REFERENCES company(company_id) ON DELETE CASCADE "+
+                ");";
+
+        public static final String SQL_DELETE_VEHICLE =
                 "DROP TABLE IF EXISTS "+ TABLE_NAME;
     }
 }
